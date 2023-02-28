955ad183e38fda63efd3bce41d7765c78d40e04f
sgrazys <s.grazys@gmail.com>
2023-02-28 17:51:16 +0200

    add del btn to remove list item

--- src/Components/UserList.jsx
+++ src/Components/UserList.jsx
@@ -1,6 +1,6 @@
 function UserList({ person, setPerson }) {
-	const delHandler = () => {
-		setPerson((s) => s.filter((s) => s.id !== person.id));
+	const delHandler = (id) => {
+		setPerson((s) => s.filter((s) => s.id !== id));
 	};
 
 	return (
@@ -8,7 +8,7 @@ function UserList({ person, setPerson }) {
 			{person.map((e) => (
 				<li key={e.id} style={{ color: e.color }}>
 					{e.name}
-					<button onClick={delHandler}>DEL</button>
+					<button onClick={() => delHandler(e.id)}>DEL</button>
 				</li>
 			))}
 		</ul>
